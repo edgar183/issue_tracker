fed28c74231dcc3833e2b283d342cc3c06df10ce
Ubuntu <ubuntu@ip-172-31-20-242.eu-west-1.compute.internal>
2019-06-07 14:05:21 +0000

    register comment table to admin panenel

--- issues/admin.py
+++ issues/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Bug, Feature
+from .models import Bug, Feature, Comment
 
 # Register your models here.
 admin.site.register(Bug)
 admin.site.register(Feature)
+admin.site.register(Comment)
