77648090ac6769ed51c95939b21e2f0eb0a83fa2
Edgars <edgars.peskaitis@gmail.com>
2019-06-07 15:27:26 +0000

    add first name field for user register form2

--- accounts/forms.py
+++ accounts/forms.py
@@ -18,7 +18,7 @@ class UserRegistrationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['first_name','username', 'email', 'password1', 'password2']
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
