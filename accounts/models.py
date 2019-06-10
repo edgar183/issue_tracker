1f75e3848a47f54a3e5be2ee48ebe5f032c81725
Edgars <edgars.peskaitis@gmail.com>
2019-06-10 10:55:45 +0000

    extend user profile from django removed in order to use django-avatar app2

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(User)
-    avatar = models.ImageField(upload_to='image/avatar', default='image/avatar/default_avatar.jpg')
-    
+# Create your models here.
