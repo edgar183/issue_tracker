27e3095d33d72860938f487a1e921c096fd6c995
Edgars <edgars.peskaitis@gmail.com>
2019-06-10 15:23:12 +0000

    initial commit for tests

--- issues/tests.py
+++ issues/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Bug
 
 # Create your tests here.
+class BugTesting(TestCase):
+    """
+    Testing the Bug models
+    """
+    def test_str(self):
+        test_bug_title= Bug(title='My First Bug')
+        self.assertEqual(str(test_bug_title), 'My First Bug')
