b41107d96a17e70e64a6e3929c109df903e1220c
Edgars <edgars.peskaitis@gmail.com>
2019-06-12 10:34:26 +0000

    add featutes url and view to sytem2

--- issues/urls.py
+++ issues/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url, include
-from .views import all_bugs
+from .views import all_bugs, all_features
 
 urlpatterns = [
-    url(r'^$', all_bugs, name='bugs'),
+    url(r'^bugs/', all_bugs, name='bugs'),
+    url(r'^features/', all_features, name='features')
     
 ]
--- issues/views.py
+++ issues/views.py
@@ -5,7 +5,7 @@ from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 def all_bugs(request):
     """
     Create a view that will return a list
-    of Bugs render them to the 'issues.html' template
+    of Bugs render them to the 'bugs.html' template
     """
     
     bug_list = Bug.objects.all().order_by('-created_date')
@@ -26,3 +26,28 @@ def all_bugs(request):
         bugs = paginator.page(paginator.num_pages)
 
     return render(request, "bugs.html", {'bugs': bugs})
+    
+def all_features(request):
+    """
+    Create a view that will return a list
+    of Features render them to the 'features.html' template
+    """
+    
+    feature_list = Feature.objects.all().order_by('-created_date')
+
+    # Pagination settings
+    page = request.GET.get('page', 1)
+    paginator = Paginator(feature_list, 2)
+    
+    try:
+        features = paginator.page(page)
+        
+    except PageNotAnInteger:
+        
+        features = paginator.page(1)
+        
+    except EmptyPage:
+        
+        features = paginator.page(paginator.num_pages)
+
+    return render(request, "features.html", {'features': features})
