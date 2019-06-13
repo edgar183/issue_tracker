e26428e713a25042808a182c3bf773d028bc9e78
Edgars <edgars.peskaitis@gmail.com>
2019-06-13 10:34:20 +0000

    editing name of link

--- issues/urls.py
+++ issues/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url, include
-from .views import all_bugs, all_features
+from .views import all_bugs, all_features, bug_form
 
 urlpatterns = [
     url(r'^bugs/', all_bugs, name='bugs'),
-    url(r'^features/', all_features, name='features')
-    
+    url(r'^features/', all_features, name='features'),
+    url(r'^newbug/', bug_form, name='new_bug'),
 ]
