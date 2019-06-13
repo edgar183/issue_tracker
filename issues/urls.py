5e4407f142810e7fb89398e93dcc5c86c47ba270
Edgars <edgars.peskaitis@gmail.com>
2019-06-13 14:01:09 +0000

    typo in lin for edit bug

--- issues/urls.py
+++ issues/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url, include
-from .views import all_bugs, all_features, create_or_edit_bug
+from .views import all_bugs, all_features, create_or_edit_bug, bug_detail
 
 urlpatterns = [
     url(r'^bugs/', all_bugs, name='bugs'),
     url(r'^features/', all_features, name='features'),
     url(r'^newbug/$', create_or_edit_bug, name='new_bug'),
+    url(r'^(?P<pk>\d+)/$', bug_detail, name='bug_detail'),
+    url(r'^(?P<pk>\d+)/edit/$', create_or_edit_bug, name='edit_bug')
 ]
