797596e4787aaa0f5da4d55c654005e9e90f6f24
Edgars <edgars.peskaitis@gmail.com>
2019-06-13 13:00:22 +0000

    change url for newbug

--- issues/urls.py
+++ issues/urls.py
@@ -4,5 +4,5 @@ from .views import all_bugs, all_features, bug_form
 urlpatterns = [
     url(r'^bugs/', all_bugs, name='bugs'),
     url(r'^features/', all_features, name='features'),
-    url(r'^newbug/', bug_form, name='new_bug'),
+    url(r'^newbug/$', bug_form, name='new_bug'),
 ]
