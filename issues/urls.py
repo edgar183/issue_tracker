4b7bfeee3f1e2c8b3cc90d7be62852bfe8473394
edgar183 <edgars.peskaitis@gmail.com>
2019-06-14 13:53:30 +0100

    edit url links as was not showing corect pages

--- issues/urls.py
+++ issues/urls.py
@@ -4,11 +4,12 @@ from .views import all_bugs, all_features, create_or_edit_bug, bug_detail, creat
 urlpatterns = [
     url(r'^bugs/', all_bugs, name='bugs'),
     url(r'^newbug/$', create_or_edit_bug, name='new_bug'),
-    url(r'^(?P<pk>\d+)/$', bug_detail, name='bug_detail'),
-    url(r'^(?P<pk>\d+)/edit/$', create_or_edit_bug, name='edit_bug'),
+    url(r'^bug/(?P<pk>\d+)/$', bug_detail, name='bug_detail'),
+    url(r'^bug/(?P<pk>\d+)/edit/$', create_or_edit_bug, name='edit_bug'),
 
     url(r'^features/', all_features, name='features'),
     url(r'^newfeature/$', create_or_edit_feature, name='new_feature'),
-    url(r'^(?P<pk>\d+)/$', feature_detail, name='bug_feature'),
-    url(r'^(?P<pk>\d+)/edit/$', create_or_edit_feature, name='edit_feature')
+    url(r'^feature/(?P<pk>\d+)/$', feature_detail, name='feature_detail'),
+    url(r'^feature/(?P<pk>\d+)/edit/$',
+        create_or_edit_feature, name='edit_feature')
 ]
