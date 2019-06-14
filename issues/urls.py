f4782ea289043664ec68a26eab9a06212a78052e
edgar183 <edgars.peskaitis@gmail.com>
2019-06-14 11:48:46 +0100

    add new url to work with features

--- issues/urls.py
+++ issues/urls.py
@@ -1,10 +1,14 @@
 from django.conf.urls import url, include
-from .views import all_bugs, all_features, create_or_edit_bug, bug_detail
+from .views import all_bugs, all_features, create_or_edit_bug, bug_detail, create_or_edit_feature, feature_detail
 
 urlpatterns = [
     url(r'^bugs/', all_bugs, name='bugs'),
-    url(r'^features/', all_features, name='features'),
     url(r'^newbug/$', create_or_edit_bug, name='new_bug'),
     url(r'^(?P<pk>\d+)/$', bug_detail, name='bug_detail'),
-    url(r'^(?P<pk>\d+)/edit/$', create_or_edit_bug, name='edit_bug')
+    url(r'^(?P<pk>\d+)/edit/$', create_or_edit_bug, name='edit_bug'),
+
+    url(r'^features/', all_features, name='features'),
+    url(r'^newfeature/$', create_or_edit_feature, name='new_feature'),
+    url(r'^(?P<pk>\d+)/$', feature_detail, name='bug_feature'),
+    url(r'^(?P<pk>\d+)/edit/$', create_or_edit_feature, name='edit_feature')
 ]
