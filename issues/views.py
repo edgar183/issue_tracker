e7f46e819b3a59d441d969c905f11809f9dd2594
edgar183 <edgars.peskaitis@gmail.com>
2019-06-14 11:48:10 +0100

    adning new views to handel feature detailes and add new feature

--- issues/views.py
+++ issues/views.py
@@ -5,69 +5,71 @@ from .models import Bug, Feature, CommentBug, CommentFeature
 from .forms import BugForm, FeatureForm, CommentBugForm, CommentFeatureForm
 
 
-
 def all_bugs(request):
     """
     Create a view that will return a list
     of Bugs render them to the 'bugs.html' template
     """
-    
+
     bug_list = Bug.objects.all().order_by('-created_date')
 
     # Pagination settings
     page = request.GET.get('page', 1)
     paginator = Paginator(bug_list, 2)
-    
+
     try:
         bugs = paginator.page(page)
-        
+
     except PageNotAnInteger:
-        
+
         bugs = paginator.page(1)
-        
+
     except EmptyPage:
-        
+
         bugs = paginator.page(paginator.num_pages)
 
     return render(request, "bugs.html", {'bugs': bugs})
-    
+
+
 def all_features(request):
     """
     Create a view that will return a list
     of Features render them to the 'features.html' template
     """
-    
+
     feature_list = Feature.objects.all().order_by('-created_date')
 
     # Pagination settings
     page = request.GET.get('page', 1)
     paginator = Paginator(feature_list, 2)
-    
+
     try:
-        features = paginator.page(page)    
+        features = paginator.page(page)
     except PageNotAnInteger:
-        
-        features = paginator.page(1)   
-    except EmptyPage:   
+
+        features = paginator.page(1)
+    except EmptyPage:
         features = paginator.page(paginator.num_pages)
     return render(request, "features.html", {'features': features})
-    
+
+
 def bug_detail(request, pk):
     """
     Create a view that returns a single
-    Bug object based on the post ID (pk) and
-    render it to the 'postdetail.html' template.
-    Or return a 404 error if the post is
+    Bug object based on the bug ID (pk) and
+    render it to the 'bugdetail.html' template.
+    Or return a 404 error if the bug is
     not found
     """
     bug = get_object_or_404(Bug, pk=pk)
     return render(request, "bugdetail.html", {'bug': bug})
-    
-@login_required()    
+
+
+@login_required()
 def create_or_edit_bug(request, pk=None):
     """
      Create a view that allows us to create
-    or edit a bug depending if the Post ID
+    or edit a bug depending if the Bug ID
     is null or not
     """
     bug = get_object_or_404(Bug, pk=pk) if pk else None
@@ -82,7 +84,36 @@ def create_or_edit_bug(request, pk=None):
     return render(request, "bugform.html", {'form': bug_form})
 
 
+def feature_detail(request, pk):
+    """
+    Create a view that returns a single
+    Feature object based on the feature ID (pk) and
+    render it to the 'featuredetail.html' template.
+    Or return a 404 error if the feature is
+    not found
+    """
+    feature = get_object_or_404(Feature, pk=pk)
+    return render(request, "featuredetail.html", {'feature': feature})
 
+
+@login_required()
+def create_or_edit_feature(request, pk=None):
+    """
+     Create a view that allows us to create
+    or edit a feature depending if the Feature ID
+    is null or not
+    """
+    feature = get_object_or_404(Feature, pk=pk) if pk else None
+    if request.method == "POST":
+        feature_form = FeatureForm(request.POST or None, instance=feature)
+        if feature_form.is_valid():
+            feature_form.instance.author = request.user
+            feature_form.instance.price = 10
+            feature = feature_form.save()
+            return redirect(feature_detail, feature.pk)
+    else:
+        feature_form = FeatureForm(instance=feature)
+    return render(request, "featureform.html", {'form': feature_form})
 
 # @login_required()
 # def edit_issue(request, pk=None):
