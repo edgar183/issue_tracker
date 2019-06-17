010fb29bb86f8f2b68e726d12f18bf761845be58
edgar183 <edgars.peskaitis@gmail.com>
2019-06-17 15:49:16 +0100

    code formating

--- issue_tracker/urls.py
+++ issue_tracker/urls.py
@@ -1,4 +1,5 @@
-"""issue_tracker URL Configuration
+"""
+issue_tracker URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
