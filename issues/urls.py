959990886fb856271dda32e5bb81a036b22bb5b8
edgar183 <edgars.peskaitis@gmail.com>
2019-06-18 10:48:13 +0100

    adding new url to upvote features

--- issues/urls.py
+++ issues/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from .views import all_bugs, all_features, create_or_edit_bug, bug_detail, create_or_edit_feature, feature_detail, create_or_edit_bug_comment, upvote_bug
+from .views import all_bugs, all_features, create_or_edit_bug, bug_detail, create_or_edit_feature, feature_detail, create_or_edit_bug_comment, upvote_bug, upvote_feature
 
 urlpatterns = [
     # bug urls
@@ -9,11 +9,12 @@ urlpatterns = [
     url(r'^bug/(?P<pk>\d+)/edit/$', create_or_edit_bug, name='edit_bug'),
     url(r'^bug/(?P<bug_pk>\d+)/new/$', create_or_edit_bug_comment, name='new_bug_comment'),
     url(r'bug/(?P<bug_pk>\d+)/(?P<pk>\d+)/edit/$', create_or_edit_bug_comment, name='edit_bug_comment'),
-    url(r'^(?P<pk>\d+)/upvote/$', upvote_bug, name='upvote_bug'),
+    url(r'bug(?P<pk>\d+)/upvote/$', upvote_bug, name='upvote_bug'),
 
     # feature urls
     url(r'^features/', all_features, name='features'),
     url(r'^newfeature/$', create_or_edit_feature, name='new_feature'),
     url(r'^feature/(?P<pk>\d+)/$', feature_detail, name='feature_detail'),
-    url(r'^feature/(?P<pk>\d+)/edit/$', create_or_edit_feature, name='edit_feature')
+    url(r'^feature/(?P<pk>\d+)/edit/$', create_or_edit_feature, name='edit_feature'),
+    url(r'^(?P<pk>\d+)/upvote/$', upvote_feature, name='upvote_feature'),
 ]
