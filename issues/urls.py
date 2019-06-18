78b0a76bbed01c1667ea4bc4f6d716576fa6f1e5
edgar183 <edgars.peskaitis@gmail.com>
2019-06-18 11:08:41 +0100

    add new links to new comment and edit comment for features

--- issues/urls.py
+++ issues/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url, include
-from .views import all_bugs, all_features, create_or_edit_bug, bug_detail, create_or_edit_feature, feature_detail, create_or_edit_bug_comment, upvote_bug, upvote_feature
+from .views import all_bugs, all_features, create_or_edit_bug, bug_detail, create_or_edit_feature, feature_detail, create_or_edit_bug_comment, upvote_bug, upvote_feature, create_or_edit_feature_comment
 
 urlpatterns = [
     # bug urls
@@ -16,5 +16,7 @@ urlpatterns = [
     url(r'^newfeature/$', create_or_edit_feature, name='new_feature'),
     url(r'^feature/(?P<pk>\d+)/$', feature_detail, name='feature_detail'),
     url(r'^feature/(?P<pk>\d+)/edit/$', create_or_edit_feature, name='edit_feature'),
+    url(r'^feature/(?P<feature_pk>\d+)/new/$', create_or_edit_feature_comment, name='new_feature_comment'),
+    url(r'feature/(?P<feature_pk>\d+)/(?P<pk>\d+)/edit/$', create_or_edit_feature_comment, name='edit_feature_comment'),
     url(r'^(?P<pk>\d+)/upvote/$', upvote_feature, name='upvote_feature'),
 ]
