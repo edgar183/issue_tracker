686ecadd60f4ba7d6852e3ee9b61c197d43aac5c
edgar183 <edgars.peskaitis@gmail.com>
2019-06-20 16:18:39 +0100

    add delete url link

--- issues/urls.py
+++ issues/urls.py
@@ -1,5 +1,18 @@
 from django.conf.urls import url, include
-from .views import all_bugs, all_features, create_or_edit_bug, bug_detail, create_or_edit_feature, feature_detail, create_or_edit_bug_comment, upvote_bug, upvote_feature, create_or_edit_feature_comment
+from .views import (
+    all_bugs, 
+    all_features, 
+    create_or_edit_bug, 
+    bug_detail, 
+    create_or_edit_feature, 
+    feature_detail, 
+    create_or_edit_bug_comment, 
+    upvote_bug, 
+    upvote_feature, 
+    create_or_edit_feature_comment,
+    delete_bug,
+    )
+
 
 urlpatterns = [
     # bug urls
@@ -9,7 +22,8 @@ urlpatterns = [
     url(r'^bug/(?P<pk>\d+)/edit/$', create_or_edit_bug, name='edit_bug'),
     url(r'^bug/(?P<bug_pk>\d+)/new/$', create_or_edit_bug_comment, name='new_bug_comment'),
     url(r'bug/(?P<bug_pk>\d+)/(?P<pk>\d+)/edit/$', create_or_edit_bug_comment, name='edit_bug_comment'),
-    url(r'bug(?P<pk>\d+)/upvote/$', upvote_bug, name='upvote_bug'),
+    url(r'bug/(?P<pk>\d+)/upvote/$', upvote_bug, name='upvote_bug'),
+    url(r'bug/(?P<pk>\d+)/delete/$', delete_bug, name='delete_bug'),
 
     # feature urls
     url(r'^features/', all_features, name='features'),
