35b70232def3f0f751e424ea97b40df10a1a7d78
edgar183 <edgars.peskaitis@gmail.com>
2019-06-20 16:18:02 +0100

    add extar field in form for userna,e

--- accounts/forms.py
+++ accounts/forms.py
@@ -19,7 +19,7 @@ class UserRegistrationForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['first_name','last_name', 'email', 'password1', 'password2']
+        fields = ['first_name','last_name','username', 'email', 'password1', 'password2']
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
