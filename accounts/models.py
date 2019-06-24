b5903b491e67f1cd3ba31d6182b44817a40f56c4
edgar183 <edgars.peskaitis@gmail.com>
2019-06-24 15:42:24 +0100

    add signals function in models file

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(default='default.jpg', upload_to='avatar')
+    image = models.ImageField(default='default.jpg', upload_to='avatar')
 
     def __str__(self):
-        return '{} Profile'.format(self.user.username)
+        return self.user.username
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+      user_profile = Profile.objects.create(user=kwargs['instance'])
+
+post_save.connect(create_profile, sender=User)
