d5be149bb448c65566c73a73a74521d39bb5696c
edgar183 <edgars.peskaitis@gmail.com>
2019-06-25 09:39:13 +0100

    adding static and media url to list if app is in ebug

--- issue_tracker/urls.py
+++ issue_tracker/urls.py
@@ -35,4 +35,7 @@ urlpatterns = [
     url(r'^checkout/', include(urls_checkout)),
     
     
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
