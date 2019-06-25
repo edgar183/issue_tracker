cab1af9b1ac86ae2b1c41948d227579e1a0adefb
edgar183 <edgars.peskaitis@gmail.com>
2019-06-25 17:05:52 +0100

    add new viesw and urls to display data

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,16 @@
 from django.conf.urls import url, include
-from .views import chart
+from .views import (
+  chart,
+  get_bug_status_json, 
+  get_feature_status_json,
+  get_bug_upvotes_json, 
+  get_feature_upvotes_json
+  )
 
 urlpatterns = [
-  url(r'^dashboard/$', chart, name='dashboard' )
+ url(r'^$', chart, name='dashboard' ),
+  url(r'^get_bug_status_json/$', get_bug_status_json, name='get_bug_status_json'),
+  url(r'^get_feature_status_json/$', get_feature_status_json, name='get_feature_status_json'),
+  url(r'^get_bug_upvotes_json/$', get_bug_upvotes_json, name='get_bug_upvotes_json'),
+  url(r'^get_feature_upvotes_json/$', get_feature_upvotes_json, name='get_feature_upvotes_json'),
 ]
