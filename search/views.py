c8a01e825a86ff2c3a4e68e2b9418854671b7e42
edgar183 <edgars.peskaitis@gmail.com>
2019-07-03 14:31:24 +0100

    format code

--- search/views.py
+++ search/views.py
@@ -5,7 +5,7 @@ from issues.models import Bug, Feature
 
 
 def search(request):
-  bugs = Bug.objects.filter(Q(title__contains=request.GET['q']) | Q(tag__contains=request.GET['q']))
-  feature = Feature.objects.filter(Q(title__contains=request.GET['q']) | Q(tag__contains=request.GET['q']))
-  results = chain(bugs, feature)
-  return render(request, "results.html", {"results": results})
+    bugs = Bug.objects.filter(Q(title__contains=request.GET['q']) | Q(tag__contains=request.GET['q']))
+    feature = Feature.objects.filter(Q(title__contains=request.GET['q']) | Q(tag__contains=request.GET['q']))
+    results = chain(bugs, feature)
+    return render(request, "results.html", {"results": results})
