ca63f64ba3956077fafb75cea3334ad8ecfed39d
edgar183 <edgars.peskaitis@gmail.com>
2019-07-08 18:23:34 +0100

    edit to get each modle out form search query

--- search/views.py
+++ search/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from itertools import chain
 from django.db.models import Q
 from issues.models import Bug, Feature
 
 
 def search(request):
-    bugs = Bug.objects.filter(Q(title__contains=request.GET['q']) | Q(tag__contains=request.GET['q']))
-    feature = Feature.objects.filter(Q(title__contains=request.GET['q']) | Q(tag__contains=request.GET['q']))
+    bugs = Bug.objects.filter(
+        Q(title__contains=request.GET['q']) | Q(tag__contains=request.GET['q']))
+    feature = Feature.objects.filter(
+        Q(title__contains=request.GET['q']) | Q(tag__contains=request.GET['q']))
     results = chain(bugs, feature)
-    return render(request, "results.html", {"results": results})
+    #bug = get_object_or_404(Bug, pk=pk)
+    #feature = get_object_or_404(Feature, pk=pk)
+    context = {
+        "results": results,
+        #"bug": bug,
+        #"feature": feature,
+    }
+    return render(request, "results.html", context)
