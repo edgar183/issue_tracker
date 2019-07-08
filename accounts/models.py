412a233537ca8ad5e683e25f7ca8daed0435feb0
edgar183 <edgars.peskaitis@gmail.com>
2019-07-08 18:21:24 +0100

    code formating

--- accounts/models.py
+++ accounts/models.py
@@ -3,9 +3,10 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from PIL import Image
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='avatar')
+    image = models.ImageField(default='default.jpg', upload_to='avatar/')
 
     def __str__(self):
         return self.user.username
