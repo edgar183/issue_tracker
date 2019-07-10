72f8689c8d9adba9ca142d6e6765d2524f56fcb6
edgar183 <edgars.peskaitis@gmail.com>
2019-07-10 14:14:34 +0100

    move signals code in to models as was cosing errors with creating new Profile for instance user

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
+from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='avatar/')
+    image = models.ImageField(default='default.jpg', upload_to='avatar')
 
     def __str__(self):
         return self.user.username
+
+
+def create_profile(sender, **kwargs):
+    if kwargs['created']:
+        user_profile = Profile.objects.create(user=kwargs['instance'])
+
+
+post_save.connect(create_profile, sender=User)
